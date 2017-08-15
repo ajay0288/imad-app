b1789c5b632d6f872b5f5c8cd80679c73fc8562d
ajay0288 <aajay0288@gmail.com>
2017-08-15 19:06:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,18 +2,18 @@ var button=document.getElementById('counter');
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readystate===XMLHttpRequest.DONE)
+        if(request.readystate==XMLHttpRequest.DONE)
         {
-            if(request.status===200)
+            if(request.status==200)
             {
                 var counter=request.responseText;
-                var span=document.getElementById('spn');
+                var span=document.getElementById('span');
                 span.innerHTML=counter.toString();
                 
             }
         }
     }
-    request.open('GET','http://aajay0288.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://aajay0288.imad.hasura-app.io/counter',true);
     request.sent(null);
 }
 
