d64c7e4c9c62067a8cf26526fbdaa49d51313f81
ajay0288 <aajay0288@gmail.com>
2017-08-24 12:15:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,35 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Pool=require('pg').Pool;
+
+var config={
+    user:'aajay0288',
+    database:'aajay0288',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+}
+
+var pool=new Pool(config);
+
+app.get('/test-db',function(req,res)
+{
+    Pool.query('select * from test',function(err,result)
+    {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
